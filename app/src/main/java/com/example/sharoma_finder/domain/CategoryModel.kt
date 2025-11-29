bef09826cc21fb6f2b7cbd2aff8090d6bd121c54
sorin2net <denisdumitriu95@gmail.com>
2025-11-29 21:44:06 +0200

    Totul e bun si frumos acum

---
+++ app/src/main/java/com/example/sharoma_finder/domain/CategoryModel.kt
@@ -0,0 +1,3 @@
+package com.example.sharoma_finder.domain
+
+data class CategoryModel()
--- app/src/main/java/com/example/sharoma_finder/screens/dashboard/Dashboard.Screen.kt
+++ app/src/main/java/com/example/sharoma_finder/screens/dashboard/Dashboard.Screen.kt
@@ -3,7 +3,7 @@ package com.example.sharoma_finder.screens.dashboard
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Scaffold
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -12,7 +12,7 @@ import com.example.sharoma_finder.R
 @Composable
 fun DashboardScreen(){
     Scaffold(
-        contentColor = colorResource(R.color.black2),
+        containerColor = colorResource(R.color.black2),
         bottomBar = { BottomBar() }
     ) { paddingValues ->
         LazyColumn (
